85ac6d6b1dfd3835743b68817c5d0b94b5d743f3
penguin <scokro7141@gmail.com>
2025-11-25 20:11:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# UGV: Peripherals
+
+This repository contains all of the contents and code of all the accessories and
+external hardware of the rover.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'ugv_peripherals'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ruthvick Bandaru, Stanley Cokro, Pedram Kayedpour, Eric Yang',
+    maintainer_email='ruthvick.bandaru@stonybrook.edu, scokro7141@gmail.com, pedram.kayedpour@stonybrook.edu, eric.yang2@.stonybrook.edu',
+    description='This package contains software for testing and controlling all of the rover peripherals.',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
